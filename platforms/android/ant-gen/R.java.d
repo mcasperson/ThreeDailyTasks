fe1f2c1e564cb25ec3dd3d44d35378b4850a0f9c
Matthew Casperson <matthewcasperson@gmail.com>
2015-09-06 18:08:53 +1000

    Updated Cordova

--- platforms/android/ant-gen/R.java.d
+++ platforms/android/ant-gen/R.java.d
@@ -1,17 +1,13 @@
 C:\Users\Matthew\workspace_jbds\ThreeLittleThings\platforms\android\ant-gen\com\threedailythings\R.java \
  : C:\Users\Matthew\workspace_jbds\ThreeLittleThings\platforms\android\res\drawable\icon.png \
-C:\Users\Matthew\workspace_jbds\ThreeLittleThings\platforms\android\res\drawable-hdpi\icon.png \
 C:\Users\Matthew\workspace_jbds\ThreeLittleThings\platforms\android\res\drawable-land-hdpi\screen.png \
 C:\Users\Matthew\workspace_jbds\ThreeLittleThings\platforms\android\res\drawable-land-ldpi\screen.png \
 C:\Users\Matthew\workspace_jbds\ThreeLittleThings\platforms\android\res\drawable-land-mdpi\screen.png \
 C:\Users\Matthew\workspace_jbds\ThreeLittleThings\platforms\android\res\drawable-land-xhdpi\screen.png \
-C:\Users\Matthew\workspace_jbds\ThreeLittleThings\platforms\android\res\drawable-ldpi\icon.png \
-C:\Users\Matthew\workspace_jbds\ThreeLittleThings\platforms\android\res\drawable-mdpi\icon.png \
 C:\Users\Matthew\workspace_jbds\ThreeLittleThings\platforms\android\res\drawable-port-hdpi\screen.png \
 C:\Users\Matthew\workspace_jbds\ThreeLittleThings\platforms\android\res\drawable-port-ldpi\screen.png \
 C:\Users\Matthew\workspace_jbds\ThreeLittleThings\platforms\android\res\drawable-port-mdpi\screen.png \
 C:\Users\Matthew\workspace_jbds\ThreeLittleThings\platforms\android\res\drawable-port-xhdpi\screen.png \
-C:\Users\Matthew\workspace_jbds\ThreeLittleThings\platforms\android\res\drawable-xhdpi\icon.png \
 C:\Users\Matthew\workspace_jbds\ThreeLittleThings\platforms\android\res\values\strings.xml \
 C:\Users\Matthew\workspace_jbds\ThreeLittleThings\platforms\android\res\xml\config.xml \
 C:\Users\Matthew\workspace_jbds\ThreeLittleThings\platforms\android\ant-build\res\drawable\icon.png \
