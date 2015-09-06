440dd7ece6509c978d9f70fd4194e6d27a77bc94
Matthew Casperson <matthewcasperson@gmail.com>
2015-09-06 19:08:05 +1000

    updated cordova

--- release.bat
+++ release.bat
@@ -1,6 +1,6 @@
 cordova build --release
 
-jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore C:\Users\Matthew\Dropbox\Common\Keys\my-release-key.keystore C:\Users\Matthew\workspace_jbds\ThreeLittleThings\platforms\android\ant-build\ThreeDailyThings-release-unsigned.apk alias_name
+jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore X:\Dropbox\Encrypted\Keys\my-release-key.keystore C:\Users\Matthew\workspace_jbds\ThreeLittleThings\platforms\android\ant-build\ThreeDailyThings-release-unsigned.apk alias_name
 
 zipalign -f -v 4 C:\Users\Matthew\workspace_jbds\ThreeLittleThings\platforms\android\ant-build\ThreeDailyThings-release-unsigned.apk C:\Users\Matthew\workspace_jbds\ThreeLittleThings\platforms\android\ant-build\ThreeDailyThings-release.apk
 
